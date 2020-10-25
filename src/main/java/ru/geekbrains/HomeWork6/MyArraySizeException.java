2964c986ee9d2c22f325a9636c80ec1e6cda81b7
Расулов Теймур Эминович <trasulov@vtb.ru>
2020-10-25 23:16:05 +0300

    HomeWork6

--- src/main/java/ru/geekbrains/HomeWork6/MyArraySizeException.java
+++ src/main/java/ru/geekbrains/HomeWork6/MyArraySizeException.java
@@ -3,7 +3,7 @@ package ru.geekbrains.HomeWork6;
 public class MyArraySizeException extends RuntimeException {
 
     public MyArraySizeException(int row, int columns, int rowAndColumnsExpected) {
-        super("Массив " + row + "x" + columns + ", а должен быть " + rowAndColumnsExpected + "x" + rowAndColumnsExpected);
+        super("Массив " + columns + "x" + row + ", а должен быть " + rowAndColumnsExpected + "x" + rowAndColumnsExpected);
     }
 
 
